3f5467a5957a28c96b79cb06578016eea9363c6d
daya18 <dayadarsan1802@gmail.com>
2022-10-12 02:59:32 +0530

    Add demo with random log generation

--- Demo/source/Demo.cpp
+++ Demo/source/Demo.cpp
@@ -1,9 +1,51 @@
+#include "LogLib/Log.hpp"
 #include "LogLib/LogWindow.hpp"
+
+#include <cstdlib>
+#include <random>
+#include <array>
+
+std::string GetLogData ( loglib::LogSeverities severity )
+{
+    switch ( severity )
+    {
+        case loglib::LogSeverities::verbose:    return "I am a verbose";
+        case loglib::LogSeverities::info:       return "I am an info";
+        case loglib::LogSeverities::warning:    return "I am a warning";
+        case loglib::LogSeverities::error:      return "I am an error";
+    }
+}
 
 int main ()
 {
-    loglib::LogWindow logWindow;
+    static constexpr std::array <loglib::LogSeverities, 4> logSeverities { 
+        loglib::LogSeverities::verbose,
+        loglib::LogSeverities::info,
+        loglib::LogSeverities::warning,
+        loglib::LogSeverities::error 
+    };
 
-    while ( 1 )
+    std::default_random_engine randomEngine;
+    std::uniform_int_distribution <int> generateLogDistribution ( 0, 30 );
+    std::uniform_int_distribution <int> logSeverityDistribution ( 0, 3 );
+
+    loglib::Log log;
+    loglib::LogWindow logWindow { log };
+
+    while ( ! logWindow.ShouldClose () )
+    {
+        if ( generateLogDistribution ( randomEngine ) == 0 )
+        {
+            auto severity = logSeverities [ logSeverityDistribution ( randomEngine ) ];
+            
+            using namespace std::string_literals;
+
+            std::string data = "Log "s + std::to_string ( log.GetEntries().size () )
+                + ": "s + GetLogData ( severity );
+
+            log << loglib::LogEntry { severity, data };
+        }
+        
         logWindow.Update ();
+    }
 }
